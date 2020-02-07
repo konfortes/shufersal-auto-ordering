b9c845efc004c3746c47e8d45f90401dd981b2b7
konfortes <ronen.konfortes@riskified.com>
2020-02-07 23:45:06 +0200

    login and navigate to fruits and vegtables

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-;((): any => {
-    console.log('yo')
+require('dotenv').config()
+import { login } from './login'
+import { navigateCategory, Category } from './navigate-category'
+import { default as puppeteer, Browser } from 'puppeteer'
+const ADDRESS = 'https://www.shufersal.co.il/online/'
+const DEBUG = process.env.NODE_ENV == 'development'
+
+const user = process.env.USER || ''
+const password = process.env.PASSWORD || ''
+;(async (): Promise<any> => {
+    const browser: Browser = await puppeteer.launch({ headless: DEBUG })
+    const page = await browser.newPage()
+    await page.setViewport({ width: 1366, height: 768 })
+    await page.goto(ADDRESS)
+
+    try {
+        const afterLoginPage = await login(page, user, password)
+        await afterLoginPage.click('a.btnContinue')
+        await afterLoginPage.waitFor('#secondMenu1')
+        const fruitsVagetablesPage = await navigateCategory(afterLoginPage, Category.FruitsAndVegetables)
+    } catch (error) {
+    } finally {
+        browser.close()
+    }
 })()
---
+++ src/login.ts
@@ -0,0 +1,15 @@
+import { Page } from 'puppeteer'
+
+export const login = async (page: Page, user: string, password: string): Promise<Page> => {
+    try {
+        await page.click('button.btnTrigger')
+        await page.type('#j_username', user)
+        await page.type('#j_password', password)
+        await page.click('button[type=submit]')
+        await page.waitFor('a.btnContinue')
+        return page
+    } catch (error) {
+        console.log(`error login. ${error}`)
+        throw error
+    }
+}
